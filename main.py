6ecc947fb6c7303ef8300620b9031ab83e9d0e18
@js407108 <j.skorupski4@student.uw.edu.pl>
2024-05-15 01:38:12 +0200

    Automatising the GA process further. Automatic saving after every generation, more information output, breaks.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import pygad
 from new_virt_env import *
 import Net
@@ -5,17 +7,25 @@ import Net
 # use the implemented VirtualEnvironment and Player classes to find an optimal player
 # this has to be done either with a Genetic Algorithm (GA) or Simulated Annealing (SA)
 # below is what it could look like with GA (copied from https://pygad.readthedocs.io/en/latest/)
+
+number_of_trials = 4  # number of games played to evaluate fitness
+
+time_of_learning = 60*60*7  # the GA will do as many generations as it can within the given time
+break_time = 60*30  # due to fear of killing my weak laptop, I will give it breaks to cool down
+time_between_breaks = 60*60*1.5
+
+display_results = False  # if the best player should be displayed
 i = 0
+generation_number = 1  # help variable to keep track of numbers of generations
 
+num_generations = 1  # 1 is enough, since this learning will be repeated multiple times
+num_parents_mating = 15
 
-num_generations = 10
-num_parents_mating = 5
-
-sol_per_pop = 12
+sol_per_pop = 50
 num_genes = 900  # number of parameters to be used in the Players' neural networks
 
-init_range_low = -0.1
-init_range_high = 0.1
+init_range_low = -0.5
+init_range_high = 0.5
 
 parent_selection_type = "sss"
 keep_parents = 1
@@ -29,32 +39,14 @@ def fitness_function(ga_instance, parameters, solution_idx):
     global i
     player = Net.Net(parameters)
     overall_fitness = 0
-
-    environment = VirtualEnvironment([player])
-    environment.calculate_full_simulation()
-    environment.fitness_function()
-    overall_fitness += player.fitness
-
-    player = Net.Net(parameters)
-    environment = VirtualEnvironment([player])
-    environment.calculate_full_simulation()
-    environment.fitness_function()
-    overall_fitness += player.fitness
-
-    player = Net.Net(parameters)
-    environment = VirtualEnvironment([player])
-    environment.calculate_full_simulation()
-    environment.fitness_function()
-    overall_fitness += player.fitness
-
-    player = Net.Net(parameters)
-    environment = VirtualEnvironment([player])
-    environment.calculate_full_simulation()
-    environment.fitness_function()
-    overall_fitness += player.fitness
+    for trial in range(number_of_trials):
+        environment = VirtualEnvironment([player])
+        environment.calculate_full_simulation()
+        environment.fitness_function()
+        overall_fitness += player.fitness
 
     i += 1
-    print(f"{i}/{num_generations*sol_per_pop} : {overall_fitness/4}")
+    print(f"{i}/{num_generations*sol_per_pop} : {overall_fitness/number_of_trials}")
     return overall_fitness/4
 
 
@@ -72,15 +64,27 @@ ga_instance = pygad.GA(num_generations=num_generations,
                        mutation_type=mutation_type,
                        mutation_percent_genes=mutation_percent_genes)
 
-ga_instance.run()
 
-ga_instance.save(f"GAresults{round(random.random()*1e6)}")
+filename = f"GAresults{round(random.random()*1e6)}"
+starting_time = time.time()
+last_break = time.time()
+while time.time() - starting_time < time_of_learning:
+    print(f"--- generation {generation_number} ---")
+    ga_instance.run()
+    ga_instance.save(filename)
+    print(f"--- generation avg fitness: {np.mean(ga_instance.last_generation_fitness)}")
+    i=0
+    generation_number += 1
+    if time.time() - last_break < time_between_breaks:
+        time.sleep(break_time)
+
 
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
 # print("Parameters of the best solution : {solution}".format(solution=solution))
 print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
-player = Net.Net(solution)
-environment = VirtualEnvironment([player], game_mode=True)
-environment.calculate_full_simulation()
-environment.fitness_function()
+if display_results:
+    player = Net.Net(solution)
+    environment = VirtualEnvironment([player], game_mode=True)
+    environment.calculate_full_simulation()
+    environment.fitness_function()
