cba95361bbb2d851c0decf7a48314743dcc29b8a
@js407108 <j.skorupski4@student.uw.edu.pl>
2024-05-29 14:32:59 +0200

    This script is now only for learning, with the option of showing a random parent after each generation.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-
 import pygad
 from new_virt_env import *
 import Net
@@ -7,11 +6,10 @@ import Net
 # PARAMETERS
 #  SCRIPT PARAMETERS:
 
-filename = "GAresults97764"  # if supplied, ga will continue learning from the supplied save
+filename = None # if supplied, ga will continue learning from the supplied save
 save_solutions = True  # this is passed to the GA instance
 
-display_results = True  # if the best player should be displayed
-teach_players = False
+display_results = False  # after each generation, a random parent will be shown
 
 # time management
 time_of_learning = 60*5  # the GA will do as many generations as it can within the given time
@@ -44,57 +42,17 @@ mutation_percent_genes = 10
 def fitness_function(ga_instance, parameters, solution_idx):
     overall_fitness = 0
     for trial in range(number_of_trials):
-        player = Net.Net(parameters)
+        net_player = Net.Net(parameters)
 
         # the seed is calculated as any function of the parameters and trial to enable replicating and viewing
-        environment = VirtualEnvironment([player], seed=np.sum(parameters) + trial)
-        environment.calculate_full_simulation()
-        environment.fitness_function()
-        overall_fitness += player.fitness
+        env = VirtualEnvironment([net_player], seed=np.sum(parameters) + trial)
+        env.calculate_full_simulation()
+        env.fitness_function()
+        overall_fitness += net_player.fitness
 
     print(f"gen:{ga_instance.generations_completed} sol:{solution_idx} fit:{overall_fitness/number_of_trials}")
     return overall_fitness/number_of_trials
 
-
-if teach_players:
-
-    if filename is None:
-        ga_instance = pygad.GA(num_generations=num_generations,
-                               num_parents_mating=num_parents_mating,
-                               fitness_func=fitness_function,
-                               sol_per_pop=sol_per_pop,
-                               num_genes=num_genes,
-                               init_range_low=init_range_low,
-                               init_range_high=init_range_high,
-                               parent_selection_type=parent_selection_type,
-                               keep_parents=keep_parents,
-                               keep_elitism=keep_elitism,
-                               crossover_type=crossover_type,
-                               mutation_type=mutation_type,
-                               mutation_percent_genes=mutation_percent_genes,
-                               save_solutions=save_solutions)
-
-        filename = f"GAresults{round(random.random()*1e6)}"
-
-    else:
-        ga_instance = pygad.load(filename)
-
-    starting_time = time.time()
-    last_break = time.time()
-    while time.time() - starting_time < time_of_learning:
-        print(f"--- generation {ga_instance.generations_completed} ---")
-        ga_instance.run()
-        ga_instance.save(filename)
-        print(f"--- generation avg fitness: {np.mean(ga_instance.last_generation_fitness)}")
-        print(f"--- generation max fitness: {np.max(ga_instance.last_generation_fitness)}")
-        if time.time() - last_break > time_between_breaks:
-            time.sleep(break_time)
-            last_break = time.time()
-
-else:  # if not teach players
-    if filename is not None:
-        ga_instance = pygad.load(filename)
-        solution, solution_fitness, solution_idx = ga_instance.best_solution()
 
 def view_player(parameters):
     if type(parameters) is not np.ndarray:
@@ -102,20 +60,51 @@ def view_player(parameters):
 
     overall_fitness = 0
     for trial in range(number_of_trials):
-        player = Net.Net(parameters)
+        net_player = Net.Net(parameters)
 
         # the seed is calculated as any function of the parameters and trial to enable replicating and viewing
-        environment = VirtualEnvironment([player], game_mode=True, seed=np.sum(parameters) + trial)
-        environment.calculate_full_simulation()
-        environment.fitness_function()
-        overall_fitness += player.fitness
-        print(f"fitness in trial {trial} : {player.fitness}")
+        env = VirtualEnvironment([net_player], game_mode=True, seed=np.sum(parameters) + trial)
+        env.calculate_full_simulation()
+        env.fitness_function()
+        overall_fitness += net_player.fitness
+        print(f"fitness in trial {trial} : {net_player.fitness}")
 
     print(f"overall fitness : {overall_fitness / number_of_trials}")
 
 
-if display_results:
-    player = Net.Net(solution)
-    environment = VirtualEnvironment([player], game_mode=True)
-    environment.calculate_full_simulation()
-    environment.fitness_function()
+if filename is None:
+    ga_instance = pygad.GA(num_generations=num_generations,
+                           num_parents_mating=num_parents_mating,
+                           fitness_func=fitness_function,
+                           sol_per_pop=sol_per_pop,
+                           num_genes=num_genes,
+                           init_range_low=init_range_low,
+                           init_range_high=init_range_high,
+                           parent_selection_type=parent_selection_type,
+                           keep_parents=keep_parents,
+                           keep_elitism=keep_elitism,
+                           crossover_type=crossover_type,
+                           mutation_type=mutation_type,
+                           mutation_percent_genes=mutation_percent_genes,
+                           save_solutions=save_solutions)
+
+    filename = f"GAresults{round(random.random()*1e6)}"
+
+else:
+    ga_instance = pygad.load(filename)
+
+starting_time = time.time()
+last_break = time.time()
+while time.time() - starting_time < time_of_learning:
+    print(f"--- generation {ga_instance.generations_completed} ---")
+    ga_instance.run()
+    ga_instance.save(filename)
+    print(f"--- generation avg fitness: {np.mean(ga_instance.last_generation_fitness)}")
+    print(f"--- generation max fitness: {np.max(ga_instance.last_generation_fitness)}")
+    if time.time() - last_break > time_between_breaks:
+        time.sleep(break_time)
+        last_break = time.time()
+
+    if display_results:
+        random_parent = ga_instance.last_generation_parents[np.random.randint(num_parents_mating)]
+        view_player(random_parent)
